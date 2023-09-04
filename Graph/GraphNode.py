c38dbaa14ede14377933d845fc0194f11c31d89e
yangyufeng04 <yangyufeng04@meituan.com>
2023-09-04 20:09:24 +0800

    update TreeNode

---
+++ Graph/GraphNode.py
@@ -0,0 +1,45 @@
+
+from graphviz import Digraph
+from typing import Dict, Optional, Set, Tuple
+
+GraphA = """
+v1:v2_2_v4_1
+v2:v5_10_v4_3
+v3:v1_4_v6_5
+v4:v3_2_v5_2_v7_4_v6_8
+v5:v7_1
+v7:v6_1
+"""
+
+
+def strToGraph(s: str) -> Dict[str, Dict[str, float]]:
+    result = {}
+    for line in s.strip().split("\n"):
+        neighborsDict = {}
+        src_node = line.split(":")[0]
+        neighbors = line.split(":")[1].split("_")
+        for i in range(len(neighbors) // 2):
+            neighborsDict[neighbors[2 * i]] = float(neighbors[2 * i + 1])
+        result[src_node] = neighborsDict
+    return result
+
+
+def plotGraph(g: Dict[str, Dict[str, float]], name: str, path: str = None, pathSet: Optional[Set[Tuple[str, str]]] = None) -> None:
+    dot = Digraph()
+    dot.attr(rankdir='LR', size='8,5')
+    dot.attr('node', shape='circle', color='lightblue2', style='filled')
+    if path:
+        pathSet = set()
+        pathArr = path.split(',')
+        for i in range(len(pathArr)-1):
+            pathSet.add((pathArr[i], pathArr[i+1]))
+
+    for src, neighbors in g.items():
+        for dst, weight in neighbors.items():
+            color = 'red' if pathSet and (src, dst) in pathSet else None
+            dot.edge(src, dst, label=str(weight), color=color)
+    dot.render(f'{name}.gv', view=True)
+
+
+if __name__ == '__main__':
+    plotGraph(strToGraph(GraphA), 'GraphA', path='v3,v1,v4,v6')
--- Tree/TreeNode.py
+++ Tree/TreeNode.py
@@ -1,3 +1,9 @@
+from typing import List, Tuple
+from graphviz import Digraph
+
+TreeA = "3,9,20,#,#,15,17"
+
+
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -5,14 +11,7 @@ class TreeNode:
         self.right = None
 
 
-"""
-      3
-    9   20
-       15 17
-"""
-
-
-def stringToTree(s: str) -> TreeNode:
+def strToTree(s: str) -> TreeNode:
     nodes = s.split(',')
     root = TreeNode(nodes[0])
     queue = [root]
@@ -30,4 +29,36 @@ def stringToTree(s: str) -> TreeNode:
     return root
 
 
-root = stringToTree("3,9,20,#,#,15,7")
+def generateEdges(root: TreeNode, result: List) -> None:
+    if root is not None:
+        if root.left:
+            result.append((root.val, root.left.val))
+            generateEdges(root.left, result)
+        if root.right:
+            result.append((root.val, root.right.val))
+            generateEdges(root.right, result)
+
+
+def plotTree(root: TreeNode, path: str = None, name: str = None) -> None:
+    dot = Digraph()
+    dot.attr(rankdir='TB', size='8,5')
+    dot.attr('node', shape='circle', color='lightblue2', style='filled')
+
+    pathSet = None
+    if path:
+        pathSet = set()
+        pathArr = path.split(',')
+        for i in range(len(pathArr) - 1):
+            pathSet.add((pathArr[i], pathArr[i + 1]))
+
+    edges: List[Tuple[str, str]] = []
+    generateEdges(root, edges)
+
+    for src, dst in edges:
+        color = 'red' if pathSet and (src, dst) in pathSet else None
+        dot.edge(src, dst, color=color)
+    dot.render(f'{name}.gv', view=True)
+
+
+treeA = strToTree(TreeA)
+plotTree(treeA, name='TreeA')
